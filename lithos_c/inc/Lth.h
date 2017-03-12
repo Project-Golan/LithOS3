844919016ceccb8bab4309bc37771f1e97ba3373
Graham <marrub.xz@gmail.com>
2017-03-12 16:34:21 -0400

    Refactoring, part 1
    Changes made over the past week or so after using this in Lithium.
    Some modules are missing and there is no makefile now.

---
+++ lithos_c/inc/Lth.h
@@ -0,0 +1,35 @@
+//-----------------------------------------------------------------------------
+//
+// Copyright © 2016-2017 Project Golan
+//
+// See "LICENSE.lithos3" for more information.
+//
+//-----------------------------------------------------------------------------
+//
+// Main header file.
+//
+//-----------------------------------------------------------------------------
+
+#ifndef lithos3__Lth_h
+#define lithos3__Lth_h
+
+// Types
+#include "Lth_types.h"
+
+// stdlib (requires: Types)
+#include "Lth_stdlib.h"
+
+// HashMap (requires: stdlib)
+#include "Lth_hashmap.h"
+
+// Token (requires: stdlib)
+#include "Lth_token.h"
+
+// TokenStream (requires: Token, stdlib)
+#include "Lth_tokenstream.h"
+
+// Manifest (requires: TokenStream, HashMap)
+#include "Lth_manifest.h"
+
+#endif//lithos3__Lth_h
+
